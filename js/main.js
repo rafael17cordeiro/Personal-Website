1b89d78ff4c402c6a7692930365f55457fd34ce6
rafael17cordeiro <rafarace17corderio@gmail.com>
2022-12-11 02:21:21 +0000

    fisrt commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let botao = document.querySelector('.menu-principal__btn');
+let menuPrincipal = document.querySelector('.menu-principal');
+
+botao.addEventListener("click",abreFechaMenu);
+
+function abreFechaMenu(evento){
+    menuPrincipal.classList.toggle('menu-principal--fechado')
+}
+
+$(document).ready(function(){
+    $('.depoimentos__caixa').slick({
+        autoplay:true,
+        arrows: false,
+        dots: true,
+    });
+});
